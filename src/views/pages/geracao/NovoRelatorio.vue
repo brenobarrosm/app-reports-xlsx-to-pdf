63259c9ab532819b7816a6e4e746a5c3c37aac24
Breno Barros <brenowbarros21@gmail.com>
2025-05-17 11:44:43 -0400

    Removendo msal e mudanças na tela inicial

--- src/views/pages/geracao/NovoRelatorio.vue
+++ src/views/pages/geracao/NovoRelatorio.vue
@@ -1,48 +1,58 @@
 <template>
-  <v-container
-      class="pa-4 d-flex justify-end"
-      style="position: absolute; top: 0; left: 0; right: 0; z-index: 10;"
-  >
-    <v-row>
-      <v-col cols="auto">
-        <v-btn
-            :text="isTokenPresent ? 'Conta Microsoft vinculada' : 'Entrar com conta Microsoft'"
-            :disabled="isTokenPresent"
-            :prepend-icon="isTokenPresent && 'mdi-account'"
-            @click="login"
-        ></v-btn>
-      </v-col>
-    </v-row>
-
-  </v-container>
-  <v-row align-content="center" justify="center" class="mt-16">
+  <v-row align-content="center" justify="center" no-gutters>
     <v-col cols="auto">
-      <v-row justify="center">
+      <v-row justify="center" class="mb-n10">
         <v-col cols="auto">
-          <h1>Gere um novo relatório</h1>
-        </v-col>
-      </v-row>
-      <v-row justify="center">
-        <v-col cols="auto">
-          <v-btn
-              text="Importar do OneDrive"
-              @click="showOneDriveDialog = true"
-              prepend-icon="mdi-microsoft-onedrive"
+          <v-icon
+              icon="mdi-file-chart-outline"
+              size="130"
               color="#1B2A41"
-              height="60"
-              width="300"
-          ></v-btn>
+          ></v-icon>
+        </v-col>
+      </v-row>
+      <v-row justify="center" class="mb-n10">
+        <v-col cols="auto">
+          <h1 class="text-xl-h3 font-weight-regular">Gere um novo relatório</h1>
+        </v-col>
+      </v-row>
+      <v-row justify="center" class="mb-2">
+        <v-col cols="auto">
+          <p class="text-xl-h6 font-weight-light">Escolha um método para começar</p>
         </v-col>
       </v-row>
       <v-row justify="center">
         <v-col cols="auto">
           <v-btn
-              text="Importar do computador"
+              text="USAR ARQUIVO DO ONEDRIVE"
+              @click="showOneDriveDialog = true"
+              color="#1B2A41"
+              height="80"
+              width="500"
+              rounded
+              class="text-lg-h5 text-uppercase font-weight-regular"
+          >
+            <template #prepend>
+              <v-icon size="36" class="mr-2">mdi-cloud</v-icon>
+            </template>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row justify="center" class="mt-n4 mb-n6">
+        <v-col cols="auto">
+          <p>ou</p>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="auto">
+          <v-btn
+              text="IMPORTE DO COMPUTADOR"
               @click="showUploadFileDialog = true"
               prepend-icon="mdi-monitor"
               color="#1B2A41"
               height="60"
-              width="300"
+              width="400"
+              variant="outlined"
+              rounded
           ></v-btn>
         </v-col>
       </v-row>
@@ -60,46 +70,17 @@
   ></upload-file>
 </template>
 <script setup>
-import {onMounted, ref} from "vue";
+import {ref} from "vue";
 import UploadFile from "@/views/pages/geracao/UploadFile.vue";
 import LinkOneDrive from "@/views/pages/geracao/LinkOneDrive.vue";
-import { msalInstance, loginRequest, initializeMsal } from "@/msalConfig";
 
 const emit = defineEmits(['fileSelected'])
 const showOneDriveDialog = ref(false)
 const showUploadFileDialog = ref(false);
-const isTokenPresent = ref(false);
-
 
 function handleUploadFile(file) {
   showUploadFileDialog.value = false;
   showOneDriveDialog.value = false;
   emit('fileSelected', file);
 }
-
-onMounted(() => {
-  const existingToken = localStorage.getItem("msal-token");
-  isTokenPresent.value = !!existingToken;
-});
-
-const login = async () => {
-  try {
-    await initializeMsal();
-
-    const loginResponse = await msalInstance.loginPopup(loginRequest);
-    const account = loginResponse.account;
-
-    const tokenResponse = await msalInstance.acquireTokenSilent({
-      ...loginRequest,
-      account,
-    });
-
-    const accessToken = tokenResponse.accessToken;
-    localStorage.setItem("msal-token", accessToken);
-    isTokenPresent.value = true;
-    console.log("Token de acesso:", accessToken);
-  } catch (error) {
-    console.error("Erro ao fazer login:", error);
-  }
-};
 </script>
